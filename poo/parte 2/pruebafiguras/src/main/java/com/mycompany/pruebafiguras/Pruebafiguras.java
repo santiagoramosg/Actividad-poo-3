d1287aa3bd21a19eab9ccd8a987254cefc4035a2
Santiago Ramos <117044142+santiagoramosg@users.noreply.github.com>
2023-09-18 19:12:41 -0500

    Add files via upload

---
+++ poo/parte 2/pruebafiguras/src/main/java/com/mycompany/pruebafiguras/Pruebafiguras.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebafiguras;
+
+/**
+ *
+ * @author santi
+ */
+public class Pruebafiguras {
+
+    public static void main(String[] args) {
+        interfaz form=new interfaz();
+        form.setVisible(true);
+    }
+}
+
